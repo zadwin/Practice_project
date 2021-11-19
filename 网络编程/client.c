3704875b113cdad639d57be193e56c3dc06b418e
zadwin <1324418453@qq.com>
2021-11-19 21:36:45 +0800

    modify socket

--- 网络编程/client.c
+++ 网络编程/client.c
@@ -13,13 +13,14 @@
 
 int main(int argc, char *argv[])
 {
+    /* 运行客户端的时候需要确定给定访问服务器的IP地址和端口号，因此这里要判断是否有三个参数。 */
     if (argc != 3)
     {
         printf("Using:./client ip port\nExample:./client 127.0.0.1 5005\n\n");
         return -1;
     }
 
-    // 第1步：创建客户端的socket。
+    // 第1步：创建客户端的socket。主要是向系统申请一个socket资源。这里也要防止系统资源耗尽，否则不会失败。
     int sockfd;
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
@@ -27,21 +28,23 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    // 第2步：向服务器发起连接请求。
-    struct hostent *h; /* 这是一个结构，返回的是主机名，IP地址以及类型等。 */
-    /* 这里获取IP地址的方式有所不同，其主要是通过这个方式，我们可以用来解析域名地址。 */
+    // 第2步：向服务器发起连接请求。gethostbyname可以将IP地址或域名换为hostent结构体，这个结构体里面包含主机名、多个IP、多个域名等等。这个函数只用于客户端。其实也就是有域名解析的功能。
+    struct hostent *h;
+
     if ((h = gethostbyname(argv[1])) == 0) // 指定服务端的ip地址。
     {
         printf("gethostbyname failed.\n");
         close(sockfd);
         return -1;
     }
+    // 套接字结构体。协议版本、IP地址和端口号。在一定程度上可以认为每一个属性都变成了内置类型。
     struct sockaddr_in servaddr;
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(atoi(argv[2])); // 指定服务端的通信端口。
-    memcpy(&servaddr.sin_addr, h->h_addr, h->h_length);
-    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) // 向服务端发起连接清求。
+    memcpy(&servaddr.sin_addr, h->h_addr, h->h_length);// C 库函数 void *memcpy(void *str1, const void *str2, size_t n) 从存储区 str2 复制 n 个字节到存储区str1。
+    // 连接请求的时候，不仅要给对方的地址等，还要告诉对方自己的通信socket等。这个函数的返回值：0表示成功，-1表示失败。失败一般可能是服务器地址错了，或者端口号错了，或则会服务的没有启动。
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) // 向服务端发起连接清求。connect函数用于将参数sockfd的socket连至serv_addr指定的服务的。因为在linux中所有打开的文件都有唯一的id号（即文件描述符）。
     {
         perror("connect");
         close(sockfd);
@@ -51,18 +54,40 @@ int main(int argc, char *argv[])
     char buffer[1024];
 
     // 第3步：与服务端通信，发送一个报文后等待回复，然后再发下一个报文。
-    for (int ii = 0; ii < 3; ii++)
+    /* 实现实时聊天。*/
+    while(1)
+    {
+	    int iret = 0;
+	    memset(buffer, 0, sizeof(buffer));
+	    printf("请输入您要发送的消息内容：");
+	    scanf("%s", buffer);
+	    if ((iret = send(sockfd, buffer, strlen(buffer), 0)) <= 0)
+	    {
+	    	perror("send"); 
+		break;
+	    }
+	    memset(buffer, 0, sizeof(buffer));
+	    printf("等待服务器发送消息...\n");
+	    if((iret = recv(sockfd, buffer, sizeof(buffer), 0)) <= 0)
+	    {
+	    	printf("iret = %d,对方已下线。\n", iret);
+		break;
+	    }
+	    printf("接收消息：%s\n", buffer);
+    }
+    /* for (int ii = 0; ii < 3; ii++)
     {
         int iret;
         memset(buffer, 0, sizeof(buffer));
-        sprintf(buffer, "这是第%d个超级女生，编号%03d。", ii + 1, ii + 1);
+	// 为了测试服务端是否一直处于等待客户端接收数据的状态。
+        sprintf(buffer, "客户端1，这是第%d个超级女生，编号%03d。", ii + 1, ii + 1);
         if ((iret = send(sockfd, buffer, strlen(buffer), 0)) <= 0) // 向服务端发送请求报文。
         {
             perror("send");
             break;
         }
         printf("发送：%s\n", buffer);
-
+	// 测试服务端发送过来的数据，是否我们可以晚接收。
         memset(buffer, 0, sizeof(buffer));
         if ((iret = recv(sockfd, buffer, sizeof(buffer), 0)) <= 0) // 接收服务端的回应报文。
         {
@@ -71,6 +96,7 @@ int main(int argc, char *argv[])
         }
         printf("接收：%s\n", buffer);
     }
+    */
 
     // 第4步：关闭socket，释放资源。
     close(sockfd);
--- 网络编程/server.c
+++ 网络编程/server.c
@@ -4,12 +4,12 @@
  */
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h> // 提供通用的文件、目录、程序及进程操作的函数
-#include <stdlib.h> // stdlib 头文件里包含了C、C++语言的最常用的系统函数
-#include <netdb.h> // 提供设置及获取域名的函数
-#include <sys/types.h> // 数据类型定义
-#include <sys/socket.h> // 提供socket函数及数据结构
-#include <arpa/inet.h>  // 里面包含了一些网络编中需要的头文件,还有一些结构体。提供IP地址转换函数。
+#include <unistd.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 int main(int argc, char *argv[])
 {
@@ -26,19 +26,16 @@ int main(int argc, char *argv[])
         perror("socket");
         return -1;
     }
-
-    // 第2步：把服务端用于通信的地址和端口绑定到socket上。
-    struct sockaddr_in servaddr; // 服务端地址信息的数据结构。这个结构体在写程序的时候更方便。
+    // 这个语句没有输出，很多时候是因为缓冲设置的问题。需要加上\n才能立即输出内容。
+    printf("中间过程...\n");
+    // 第2步：把服务端用于通信的地址和端口绑定到socket上。服务端才需要将自己的Ip端口等进行绑定。因为它是等待其他客户的连接。
+    struct sockaddr_in servaddr; // 服务端地址信息的数据结构。
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;                // 协议族，在socket编程中只能是AF_INET。
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // 任意ip地址。
-    // servaddr.sin_addr.s_addr = inet_addr("192.168.190.134"); // 指定ip地址。这里也可以通过函数将给的字符串IP地址转换为结构体中的类型。
+    // servaddr.sin_addr.s_addr = inet_addr("192.168.190.134"); // 指定ip地址。
     servaddr.sin_port = htons(atoi(argv[1])); // 指定通信端口。
-    /* 在bind函数中，要求的套接字结构体是老的版本，因此我们这里需要将它强制转换一下。 */
-    /* 同一个端口不能被绑定两次。 端口是从0——65535，有一些端口是系统特定的，并且有些端口普通用户不能使用。
-        服务端程序的端口释放后可能出与TIME_WAIT状态，等待两分钟后才能再次使用。当然也有特定的函数让端口释放后立即就可以再次使用。
-
-    */
+    /* 服务端把用于通信的地址和端口号绑定到socket上。成功返回0，错误返回-1。如果绑定的地址错误、或端口已被占用，则会报错，否则一般不会返回错误。*/
     if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0)
     {
         perror("bind");
@@ -46,45 +43,67 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    // 第3步：把socket设置为监听模式。设置了监听模式后，其实就可以进行连接。
+    printf("监听模式开始...\n");
+    // 第3步：把socket设置为监听模式。listen函数把主动连接socket变为被动连接的socket，使得这个socket可以接受其他socket连接请求，从而成为一个服务端的socket。socket是已经被bind绑定过的socket，socket函数返回的socket是一个主动连接的socket，在服务端的编程中，程序员希望这个socket可以接收外来请求，也就是被动等待客户端来连接。由于系统默认认为一个socket是主动连接的，所以需要通过某种方式来告诉系统，通过调用listen来完成这件事。第二个参数backlog代表等待队列的最大长度。
     if (listen(listenfd, 5) != 0)
     {
         perror("listen");
         close(listenfd);
         return -1;
     }
-
+    printf("监听模式结束。\n");
+    
     // 第4步：接受客户端的连接。
     int clientfd;                             // 客户端的socket。
     int socklen = sizeof(struct sockaddr_in); // struct sockaddr_in的大小
-    struct sockaddr_in clientaddr;            // 客户端的地址信息。还没有接收的。
-    /* 这里accept是指从准备好的连接队列中获取一个请求，如果队列为空，accept函数将阻塞等待直到有新的连接才会进行后面的操作。*/
+    struct sockaddr_in clientaddr;            // 客户端的地址信息。
+    printf("开始接受监听...\n");  // 当服务器开始运行时，会等待客服端的连接。
+    // sleep(10);
+    // accept函数用来等待来自客户端的连接请求,虽然出与监听状态时可能已经接收了客户端的请求
+    // 	但是还是要等到accpet函数接收了之后，客户端才会发送请求过来。这也应该是写并发程序的切入点。
+    /* 这里的代码是为了测试服务器用来接收客户端请求的时候是以何种形式。 */
+    /*int count = 2;
+    while(count > 0)
+    {
+    	clientfd = accept(listenfd, (struct sockaddr *)&clientaddr, (socklen_t *)&socklen);
+	printf("客户端（%s）已连接。\n", inet_ntoa(clientaddr.sin_addr));
+  	count--;
+    }*/
+    // 等待接收。
     clientfd = accept(listenfd, (struct sockaddr *)&clientaddr, (socklen_t *)&socklen);
     printf("客户端（%s）已连接。\n", inet_ntoa(clientaddr.sin_addr));
 
     // 第5步：与客户端通信，接收客户端发过来的报文后，回复ok。
-    char buffer[1024];
-    while (1)
+    char buffer[1024]; // 这里的buffer只是为了使用它显示的取数据，还不是真正的socket缓冲区。
+    while (1)  // 服务端虽然这里是一个循环，但是由于后面的if语句中的recv会判断是否客户端还连接。如果不连接就会终止该程序。
     {
         int iret;
         memset(buffer, 0, sizeof(buffer));
-        if ((iret = recv(clientfd, buffer, sizeof(buffer), 0)) <= 0) // 接收客户端的请求报文。
+        // 开始接收客户端的报文。
+	printf("等待客户端发送消息报文...\n");// 一定要特别注意c语言的输出缓冲区问题。如果没有\n那么有可能就不会立即输出。
+	if ((iret = recv(clientfd, buffer, sizeof(buffer), 0)) <= 0) // 接收客户端的请求报文。
         {
             printf("iret=%d\n", iret);
             break;
         }
-        printf("接收：%s\n", buffer);
-
-        strcpy(buffer, "ok");
-        if ((iret = send(clientfd, buffer, strlen(buffer), 0)) <= 0) // 向客户端发送响应结果。
+        printf("接收消息：%s\n", buffer);
+	if(strcmp(buffer, "Bye") == 0 )
+	{
+	    printf("聊天结束...\n");
+	    break;
+	}
+        // strcpy(buffer, "ok"); // 该函数的功能是，把一个字符串复制到另一个字符串中。
+	memset(buffer, 0, sizeof(buffer));
+        printf("请输入您要发送的消息：");
+	scanf("%s", buffer);
+	if ((iret = send(clientfd, buffer, strlen(buffer), 0)) <= 0) // 向客户端发送响应结果。
         {
             perror("send");
             break;
         }
-        printf("发送：%s\n", buffer);
     }
-
-    // 第6步：关闭socket，释放资源。
+    // 第6步：关闭s
+    // ocket，释放资源。
     close(listenfd);
     close(clientfd);
 }
