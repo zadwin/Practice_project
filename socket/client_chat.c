18347162203d3e993aa4704e0feed7eece60039a
zadwin <1324418453@qq.com>
2021-11-21 17:23:20 +0800

    commit vim命令

--- socket/client_chat.c
+++ socket/client_chat.c
@@ -16,6 +16,7 @@ void client_chat(int clientfd)
         memset(buffer, 0, sizeof(buffer));
         printf("请输入您要发送的消息内容：");
         scanf("%s", buffer);
+        // 同样也会存在和recv相同的结果，因为不能一次性写入这么多内容，可能是因为缓冲区的问题。
         if ((iret = send(clientfd, buffer, strlen(buffer), 0)) <= 0)
         {
             perror("send");
@@ -23,6 +24,8 @@ void client_chat(int clientfd)
         }
         memset(buffer, 0, sizeof(buffer));
         printf("等待服务器发送消息...\n");
+        // recv函数因为要指定接收数据的大小，因此如果一个包过大，需要用循环的方式提取。不能完整的读取数据。
+        // 因此，可以自己构造一个函数，用以去接收完整的数据。
         if ((iret = recv(clientfd, buffer, sizeof(buffer), 0)) <= 0)
         {
             printf("iret = %d,对方已下线。\n", iret);
